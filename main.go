5bb776139af7ef7866fb33253e203710de95d51c
myuon <ioi.joi.koi.loi@gmail.com>
2024-12-04 00:52:42 +0900

    refactor: remove writeHeader

--- main.go
+++ main.go
@@ -3,14 +3,50 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	"cloud.google.com/go/speech/apiv1/speechpb"
 	"github.com/gordonklaus/portaudio"
 )
+
+func RecordMicStream(writer io.Writer) error {
+	recordSeconds := 5
+	sampleRate := 44100
+	numChannels := 1
+	framesPerBuffer := 64
+
+	// 入力ストリームの作成
+	in := make([]int16, framesPerBuffer*numChannels)
+	stream, err := portaudio.OpenDefaultStream(numChannels, 0, float64(sampleRate), framesPerBuffer, func(inBuf, outBuf []int16) {
+		copy(in, inBuf)
+		// WAVファイルにデータを書き込む
+		binary.Write(writer, binary.LittleEndian, in)
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer stream.Close()
+
+	// 録音開始
+	err = stream.Start()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("録音中...")
+	time.Sleep(time.Duration(recordSeconds) * time.Second)
+	err = stream.Stop()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("録音完了")
+
+	return nil
+}
 
 func RunSpeechToText(f io.Reader) error {
 	ctx := context.Background()
